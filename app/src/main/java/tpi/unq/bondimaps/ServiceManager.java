9e04dd184cb08fbf1221614b02d24447f3f5edeb
Luis Zagarella <zagarojo23@gmail.com>
2016-09-25 22:45:57 -0300

    sprint 0 completed

---
+++ app/src/main/java/tpi/unq/bondimaps/ServiceManager.java
@@ -0,0 +1,55 @@
+package unq.tpi.bondimaps;
+
+import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.StrictMode;
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class ServiceManager {
+
+    private Activity activity;
+
+    public ServiceManager(Activity activity){
+        this.activity = activity;
+    }
+
+    public JSONObject getResource(String url) throws Exception {
+
+        JSONObject responseObject = null;
+
+        ConnectivityManager connMgr = (ConnectivityManager)activity.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if(networkInfo != null && networkInfo.isConnected()){
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection)obj.openConnection();
+            con.setRequestMethod("GET");
+            int responseCode = con.getResponseCode();
+            Log.i("responseCode ","- " + responseCode);
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while((inputLine = in.readLine()) != null){
+                response.append(inputLine);
+            }
+            in.close();
+            responseObject = new JSONObject(response.toString());
+        }
+        else{
+            Log.i("error", "No connection");
+        }
+
+        return responseObject;
+    }
+
+}
